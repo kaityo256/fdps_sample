c830adc71a93e8d5090a0eb1d002e60e98a79166
H. Watanabe <kaityo@users.sourceforge.jp>
2017-03-01 17:16:38 +0900

    removes unused code

--- step1/test.cpp
+++ step1/test.cpp
@@ -1,6 +1,5 @@
 //------------------------------------------------------------------------
 #include <stdio.h>
-#include <iostream>
 #include <particle_simulator.hpp>
 int
 main(int argc, char **argv) {
